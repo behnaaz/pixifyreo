15ffdeaa2c50ab76efc27db1b65b395bf73bc97a
changib <behnaz.changizi@rabobank.nl>
2019-12-04 13:13:53 +0100

    extract js into file add channel

--- reo.js
+++ reo.js
@@ -1,3 +1,88 @@
+var drawingMode = 'Node';
+const graphics = new PIXI.Graphics();
+const app = new PIXI.Application({
+    antialias: true
+});
+var points = [];
+
+function windowload() {
+    document.body.appendChild(app.view);
+
+    app.renderer.plugins.interaction.on('mousedown', onPointerDown);
+
+    var rad = 10;
+    var nodes = [
+        [20, 30, 'a', 'left'],
+        [100, 400, 'b', 'up'],
+        [130, 300, 'c', 'right']
+    ];
+    var channels = [
+        [0, 1, 'sync'],
+        [1, 2, 'fifo']
+    ];
+    // Rectangle 2
+    graphics.lineStyle(2, 0xFFFFFF, 1);
+    graphics.beginFill(0xAA4F08);
+    graphics.drawRect(530, 50, 140, 100);
+    graphics.endFill();
+
+    // Circle + line style 1
+    for (var i = 0; i < nodes.length; i++) {
+        graphics.lineStyle(2, 0xFEEB77, 1);
+        graphics.beginFill(0x650A5A, 1);
+        graphics.drawCircle(nodes[i][0], nodes[i][1], rad);
+        graphics.endFill();
+    }
+
+    for (var i = 0; i < channels.length; i++) {
+
+        const realPath = new PIXI.Graphics();
+        realPath.lineStyle(2, 0xFFFFFF, 1);
+        var src = nodes[channels[i][0]];
+        var dest = nodes[channels[i][1]];
+        realPath.moveTo(src[0], src[1]);
+        realPath.lineTo(dest[0], dest[1]);
+
+        realPath.position.x = rad;
+        realPath.position.y = rad;
+
+        app.stage.addChild(realPath);
+    }
+
+
+    app.stage.addChild(graphics);
+}
+
+function mode(mod) {
+    window.drawingMode = (mod === 'Node') ? 'Node' : 'Channel';
+}
+
+
+function onPointerDown(event) {
+
+    const newPosition = event.data.global;
+
+    if (window.drawingMode === 'Node') {
+        // points = points.push(newPosition.x);
+        graphics.drawCircle(newPosition.x, newPosition.y, 20);
+        console.log(points);
+    } else {
+        channel([40, 30], [newPosition.x, newPosition.y]);
+    }
+}
+
+function channel(src, dest) {
+    const realPath = new PIXI.Graphics();
+    realPath.lineStyle(2, 0xFFFFFF, 1);
+
+    realPath.moveTo(src[0], src[1]);
+    realPath.lineTo(dest[0], dest[1]);
+    //realPath.angle = 2;
+
+    app.stage.addChild(realPath);
+}
+
+/*
 	var drawer = new function {	
 		const app = new PIXI.Application({
 			antialias: true
@@ -49,4 +134,4 @@
 
 
 		app.stage.addChild(graphics);
-	};
+	};*/
